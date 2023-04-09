1f827a79a68562c7e33e4f656b3449026ba558ee
Chsalinetti <cws2525@rit.edu>
2023-04-09 17:05:56 -0400

    progress made on 3.1

--- src/seleniumTest/DriverPreference.java
+++ src/seleniumTest/DriverPreference.java
@@ -3,5 +3,5 @@ package seleniumTest;
 public class DriverPreference {
     // Change this to utilize your desired driver
     // Makes it easier to test locally across different operating systems
-    public static final String driverName = "chromedriver_mac";
+    public static final String driverName = "chromedriver_win.exe";
 }
--- src/seleniumTest/TigerCenterTest.java
+++ src/seleniumTest/TigerCenterTest.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.concurrent.TimeUnit;
 
 class TigerCenterClass {
@@ -27,7 +30,7 @@ class TigerCenterClass {
 
     @AfterEach
     void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
@@ -36,6 +39,34 @@ class TigerCenterClass {
         WebElement classButton = driver.findElement(By.xpath("//*[@id=\"angularApp\"]/app-root/div[2]/mat-sidenav-container[2]/mat-sidenav-content/div[2]/landing-page/div/div/div/div/div[4]/a[1]"));
         assertEquals("Class Search", classButton.getText());
         classButton.click();
+    }
+    
+    //written by cws2525
+    @Test
+    public void testClassSearchSWEN352() throws Exception {
+    	
+    	//driver.findElement(By.xpath(""));
+    	
+        driver.get(baseUrl+"tigercenter.rit.edu/tigerCenterApp/landing");
+        WebElement classButton = driver.findElement(By.xpath("//*[@id=\"angularApp\"]/app-root/div[2]/mat-sidenav-container[2]/mat-sidenav-content/div[2]/landing-page/div/div/div/div/div[4]/a[1]"));
+        //assertEquals("Class Search", classButton.getText());
+        classButton.click();
+        //select term
+        new Select (driver.findElement(By.xpath("//*[@id=\"hideTerm\"]/div/select"))).selectByVisibleText("2023-24 Fall (2231)");
+        //click searchbar
+        driver.findElement(By.xpath("//*[@id=\"ng2Completer\"]/div/input")).clear();
+        //type swen 352
+        driver.findElement(By.xpath("//*[@id=\"ng2Completer\"]/div/input")).sendKeys("swen 352");
+        //enter
+        driver.findElement(By.xpath("//*[@id=\"classSearchContainer\"]/div[2]/form/div/button")).click();
+        //driver.findElement(By.xpath(""));
+        //get list of webelements from class details table
+        WebElement classDetails = driver.findElement(By.xpath("//*[@id=\"classSearchContainer\"]/div[2]/div[4]/div[5]/app-class-search-row/div"));
+        //go through webelements list and print days, time, location, instructor
+        System.out.println(classDetails);
+        //3 second delay
+        new WebDriverWait(driver, 5);
+        
     }
 
 }
